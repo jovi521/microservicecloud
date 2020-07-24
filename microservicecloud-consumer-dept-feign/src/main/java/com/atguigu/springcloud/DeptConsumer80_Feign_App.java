4bdd7834825d198a72a286bae89c4bd8625cd333
jovi521 <506997994@qq.com>
2020-07-24 09:51:52 +0800

    项目重构，版本升级，代码修改

--- microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
+++ microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
@@ -3,13 +3,11 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.atguigu.springcloud"})
 @EnableEurekaClient
 @EnableFeignClients(basePackages = {"com.atguigu.springcloud"})
-@ComponentScan("com.atguigu.springcloud")
 public class DeptConsumer80_Feign_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer80_Feign_App.class, args);
